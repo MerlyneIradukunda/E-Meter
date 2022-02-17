ca81637f7940ab23849a0ad4b8e5761c2a0b8e57
MerlyneIradukunda <merlyneirad05@gmail.com>
2022-02-17 14:18:21 +0200

    Feat: setup the front end

---
+++ e-meter/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <h4 className="text-emerald-400 font-mono underline">
+    Hello world!
+  </h4>
+  );
+}
+
+export default App;
